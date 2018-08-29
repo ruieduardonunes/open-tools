d7983d51841caad44cabdbffbd188929398263da
ruieduardonunes <github@ruieduardo.com>
2018-08-29 19:34:20 +0200

    tests

--- js/arena.js
+++ js/arena.js
@@ -34,22 +34,7 @@ function populatePage(data) {
 function test() {
   fetch(
     "http://dev.are.na/oauth/authorize?client_id=6ce6938e5e24c827fdb6acb55a92e1e006b165266ae2b1d6fc4cebd89aea81ce&redirect_uri=https://opentools.design/&response_type=code"
-  )
-    .then(function(response) {
-      return response;
-    })
-    .then(function(key) {
-      var url =
-        "https://dev.are.na/oauth/token?client_id=6ce6938e5e24c827fdb6acb55a92e1e006b165266ae2b1d6fc4cebd89aea81ce&client_secret=14af41a719258af9a23dd256b3e65314d9ed0d15caae0ee08a921241fa1d44d4&code=" +
-        key +
-        "&grant_type=authorization_code&redirect_uri=https://opentools.design/";
-      fetch(url, {
-        method: "POST", // or 'PUT'
-        headers: {
-          "Content-Type": "application/json"
-        }
-      })
-        .then(res => res.json())
-        .then(response => console.log("Success:", JSON.stringify(response)));
-    });
+  ).then(function(response) {
+    return response;
+  });
 }
