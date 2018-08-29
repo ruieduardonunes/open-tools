42497d56c98718947c3aba5ce569454d6a7a30ba
ruieduardonunes <github@ruieduardo.com>
2018-08-29 21:22:52 +0200

    tests

--- js/arena.js
+++ js/arena.js
@@ -45,6 +45,19 @@ function test2() {
   if (hasCode) {
     var url = new URL(currentURL.href);
     var code = url.searchParams.get("code");
-    console.log(code);
+    getKey(code);
   }
+}
+
+function getCode(code) {
+  var url =
+    "https://dev.are.na/oauth/token?client_id=6ce6938e5e24c827fdb6acb55a92e1e006b165266ae2b1d6fc4cebd89aea81ce&client_secret=14af41a719258af9a23dd256b3e65314d9ed0d15caae0ee08a921241fa1d44d4&code=" +
+    code +
+    "&grant_type=authorization_code&redirect_uri=https://opentools.design/";
+
+  fetch(url, {
+    method: "POST"
+  })
+    .then(res => res.json())
+    .then(response => console.log("Success:", JSON.stringify(response)));
 }
