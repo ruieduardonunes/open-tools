129a9040c650e25dd3201a0717cfa56b00faa73b
Gomes Nunes <github@ruieduardo.com>
2018-10-29 20:55:22 +0100

    new layout

--- js/arena.js
+++ js/arena.js
@@ -29,96 +29,45 @@ function populatePage(data) {
   collumn = document.getElementsByClassName("collumn");
 
   for (let i = 0; i < data.contents.length; i++) {
-    if (i == 0) {
-      var hero = document.getElementById("featured-hero");
-      var link = document.createElement("a");
-      var image = document.createElement("img");
-      var par = document.createElement("p");
-      var title = document.createElement("h3");
-      var desc = document.createElement("h6");
+    var container = document.getElementsByClassName("link-wrapper")[0];
+    var link = document.createElement("a");
+    var image = document.createElement("img");
+    var par = document.createElement("p");
+    var title = document.createElement("h6");
+    var wrapper = document.createElement("div");
 
-      if (data.contents[i].source) {
-        link.setAttribute("href", data.contents[i].source.url);
-      }
-      // if (data.contents[i].image) {
-      //   image.src = data.contents[i].image.large.url;
-      // }
-      par.innerHTML = "added by" + " " + data.contents[0].connected_by_username;
-      title.innerHTML = data.contents[0].title;
-      desc.innerHTML = data.contents[0].description;
+    link.setAttribute("target", "_blank");
 
-      link.appendChild(image);
-      link.appendChild(par);
-      link.appendChild(title);
-      link.appendChild(desc);
-      hero.appendChild(link);
-    } else if (i == 1 || i == 2) {
-      var container = collumn[1];
-      var link = document.createElement("a");
-      var image = document.createElement("img");
-      var par = document.createElement("p");
-      var title = document.createElement("h6");
-      var wrapper = document.createElement("div");
-
-      if (data.contents[i].source) {
-        link.setAttribute("href", data.contents[i].source.url);
-      }
-      // if (data.contents[i].image) {
-      //   image.src = data.contents[i].image.display.url;
-      // }
-      par.innerHTML = "added by" + " " + data.contents[i].connected_by_username;
-      title.innerHTML = data.contents[i].title;
-
-      link.appendChild(image);
-      link.appendChild(par);
-      link.appendChild(title);
-      wrapper.appendChild(link);
-      container.appendChild(wrapper);
-    } else if (i == 3 || i == 4) {
-      var container = collumn[2];
-      var link = document.createElement("a");
-      var image = document.createElement("img");
-      var par = document.createElement("p");
-      var title = document.createElement("h6");
-      var wrapper = document.createElement("div");
-
-      if (data.contents[i].source) {
-        link.setAttribute("href", data.contents[i].source.url);
-      }
-      // if (data.contents[i].image) {
-      //   image.src = data.contents[i].image.display.url;
-      // }
-      par.innerHTML = "added by" + " " + data.contents[i].connected_by_username;
-      title.innerHTML = data.contents[i].title;
-
-      link.appendChild(image);
-      link.appendChild(par);
-      link.appendChild(title);
-      wrapper.appendChild(link);
-      container.appendChild(wrapper);
-    } else if (i >= 5) {
-      console.log("this is" + i);
-      var container = document.getElementsByClassName("link-wrapper")[0];
-      var link = document.createElement("a");
-      var image = document.createElement("img");
-      var par = document.createElement("p");
-      var title = document.createElement("h6");
-      var wrapper = document.createElement("div");
-
-      if (data.contents[i].source) {
-        link.setAttribute("href", data.contents[i].source.url);
-      }
-      // if (data.contents[i].image) {
-      //   image.src = data.contents[i].image.display.url;
-      // }
-      par.innerHTML = "added by" + " " + data.contents[i].connected_by_username;
-      title.innerHTML = data.contents[i].title;
-
-      link.appendChild(image);
-      link.appendChild(par);
-      link.appendChild(title);
-      wrapper.appendChild(link);
-      container.appendChild(wrapper);
+    if (data.contents[i].source) {
+      link.setAttribute("href", data.contents[i].source.url);
+    } else if (data.contents[i].class == "Channel") {
+      link.setAttribute(
+        "href",
+        "https://www.are.na/" +
+          data.contents[i].user.slug +
+          "/" +
+          data.contents[i].slug
+      );
+    } else if (data.contents[i].class == "Attachment") {
+      link.setAttribute("href", data.contents[i].attachment.url);
     }
+    if (data.contents[i].image) {
+      image.src = data.contents[i].image.display.url;
+    } else if (data.contents[i].class == "Channel") {
+      image.src = "img/icons/link-icon.svg";
+      image.style.backgroundColor = "var(--accentColor)";
+    } else {
+      image.src = "img/icons/link-icon.svg";
+      image.style.backgroundColor = "var(--accentColor)";
+    }
+
+    par.innerHTML = "added by" + " " + data.contents[i].connected_by_username;
+    title.innerHTML = data.contents[i].title;
+
+    link.appendChild(image);
+    link.appendChild(par);
+    link.appendChild(title);
+    wrapper.appendChild(link);
+    container.appendChild(wrapper);
   }
 }
