7b916542c6e88c0a72022f371b9401e424875404
Gomes Nunes <github@ruieduardo.com>
2018-10-30 21:54:24 +0100

    ui improvement

--- js/arena.js
+++ js/arena.js
@@ -54,6 +54,8 @@ function populatePage(data) {
       );
     } else if (data.contents[i].class == "Attachment") {
       link.setAttribute("href", data.contents[i].attachment.url);
+    } else if (data.contents[i].class == "Image") {
+      link.setAttribute("href", data.contents[i].image.original.url);
     }
     if (data.contents[i].image) {
       image.src = data.contents[i].image.display.url;
@@ -78,6 +80,7 @@ function populatePage(data) {
 
 pageHero = document.getElementsByClassName("title")[0];
 pageTitle = document.getElementById("page-title");
+pageLogo = document.getElementById("page-logo");
 nav = document.getElementsByTagName("nav")[0];
 
 window.onscroll = function() {
@@ -85,10 +88,20 @@ window.onscroll = function() {
   if (pageHero.getBoundingClientRect().top <= 0) {
     pageTitle.style.opacity = 0;
     nav.style.borderBottom = "";
+    pageLogo.innerHTML = "opentools.design";
   }
   //BOTTOM
   if (pageHero.getBoundingClientRect().bottom <= 0) {
     pageTitle.style.opacity = 1;
     nav.style.borderBottom = "1px solid #f2f2f2";
+
+    function mobile(x) {
+      if (x.matches) {
+        pageLogo.innerHTML = "Back";
+      }
+    }
+    var x = window.matchMedia("(max-width: 510px)");
+    mobile(x);
+    x.addListener(mobile);
   }
 };
