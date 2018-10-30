a9df36ee97ca5942fec1ae2adeb1b2b63b9748a4
Gomes Nunes <github@ruieduardo.com>
2018-10-30 22:55:21 +0100

    minor improvements

--- js/arena.js
+++ js/arena.js
@@ -77,31 +77,3 @@ function populatePage(data) {
     container.appendChild(wrapper);
   }
 }
-
-pageHero = document.getElementsByClassName("title")[0];
-pageTitle = document.getElementById("page-title");
-pageLogo = document.getElementById("page-logo");
-nav = document.getElementsByTagName("nav")[0];
-
-window.onscroll = function() {
-  //TOP
-  if (pageHero.getBoundingClientRect().top <= 0) {
-    pageTitle.style.opacity = 0;
-    nav.style.borderBottom = "";
-    pageLogo.innerHTML = "opentools.design";
-  }
-  //BOTTOM
-  if (pageHero.getBoundingClientRect().bottom <= 0) {
-    pageTitle.style.opacity = 1;
-    nav.style.borderBottom = "1px solid #f2f2f2";
-
-    function mobile(x) {
-      if (x.matches) {
-        pageLogo.innerHTML = "Back";
-      }
-    }
-    var x = window.matchMedia("(max-width: 510px)");
-    mobile(x);
-    x.addListener(mobile);
-  }
-};
